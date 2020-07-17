a2a339540feaf94371f68ca0ef679a1d53111458
xiaobai <xiaobai.pt@gmail.com>
2020-07-17 17:05:56 +0800

    Initial Commit

---
+++ Landmarks-SwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Apple on 2020/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks-SwiftUI/Landmarks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_SwiftUIApp.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Apple on 2020/7/17.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
