fb1f77187da17aab04ec1748491c415c82dd3673
yellow <yellowchao91@163.com>
2020-07-20 11:17:34 +0800

    SwiftUI 创建和组合视图

---
+++ Landmarks-SwiftUI/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks-SwiftUI
+//
+//  Created by Apple on 2020/7/20.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+       Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
