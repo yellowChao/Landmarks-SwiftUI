653284e65ecc0a0c5831675f4787bc1cda7bddd1
yellow <yellowchao91@163.com>
2021-02-05 17:58:34 +0800

    update

---
+++ Landmarks-SwiftUI/Profile/ProfileHost.swift
@@ -0,0 +1,20 @@
+//
+//  PorfileHost.swift
+//  Landmarks-SwiftUI
+//
+//  Created by 黄智超 on 2021/1/21.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
